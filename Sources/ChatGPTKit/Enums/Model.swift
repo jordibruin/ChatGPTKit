3979c7444bb5a8d7228a27554ac4dda803401ecc
Jordi Bruin <jordibruin@gmail.com>
2023-03-17 22:18:15 +0100

    Update Model.swift

--- Sources/ChatGPTKit/Enums/Model.swift
+++ Sources/ChatGPTKit/Enums/Model.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 // Enums
-public enum Model: String, CaseIterable {
+public enum Model: String, CaseIterable, Identifiable {
     // Base
     case turbo = "gpt-3.5-turbo"
     case turbo31 = "gpt-3.5-turbo-0301"
     case gpt4 = "gpt-4"
+    
+    var id: String { self.rawValue }
 }
